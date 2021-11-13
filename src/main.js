a0b8ba241f803f5aacef107c9775caab7a7e2649
Antonio Vivace <avivace4@gmail.com>
2021-11-13 17:23:42 +0100

    Import and configure a bunch of stuff

--- src/main.js
+++ src/main.js
@@ -1,26 +1,38 @@
 import { createApp } from 'vue'
 
+// Local components 
 import App from './App.vue'
-import Game from './components/Game.vue'
+import Game from './components/Game'
 import Landing from './components/Landing'
 import List from './components/List'
 
+import Footer from './components/Footer'
+
+// Tools
 import axios from 'axios'
 import * as VueRouter from 'vue-router';
 
+// PrimeVue UI components
 import PrimeVue from 'primevue/config';
 import Menubar from 'primevue/menubar';
 import InputText from 'primevue/inputtext';
 import Carousel from 'primevue/carousel';
 import Button from 'primevue/button';
+import Tag from 'primevue/tag';
+import Chip from 'primevue/chip';
+import Slider from 'primevue/slider';
 
+// Directives
+import Tooltip from 'primevue/tooltip';
 
+// Theming
 import "primevue/resources/themes/mdc-light-indigo/theme.css"
 import "primevue/resources/primevue.min.css"
 import "primeicons/primeicons.css"
 import 'primeflex/primeflex.css';
 import './assets/style.scss';
 
+// Routing
 const routes = [
     { path: '/game/:slug', component: Game },
     { path: '/', component: Landing },
@@ -33,11 +45,28 @@ const router = VueRouter.createRouter({
     routes, // short for `routes: routes`
 })
 
-createApp(App)
-    .use(router)
-    .use(PrimeVue)
-    .component("Menubar", Menubar)
-    .component("InputText", InputText)
-    .component("Carousel", Carousel)
-    .component("Button", Button)
-    .mount('#app')
+// Set up Vue app
+
+// Returns app instance
+app = createApp(App)
+
+app.directive('tooltip', Tooltip);
+
+// Returns root component instance
+app.use(router)
+   .use(PrimeVue)
+
+// Enable components
+app.component("Menubar", Menubar)
+   .component("InputText", InputText)
+   .component("Carousel", Carousel)
+   .component("Button", Button)
+   .component("Tag", Tag)
+   .component("Chip", Chip)
+   .component("Slider", Slider)
+   .component("Footer", Footer)
+   .mount('#app')
+
+// Base API endpoint
+//  Must point to an instance of the HHub-API (https://github.com/gbdev/homebrewhub)
+app.config.globalProperties.baseAPI = 'https://hh3.gbdev.io'
