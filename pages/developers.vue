7b50790014907d05858698a03547020e6f1daee7
Antonio Vivace <antonio.vivace@cern.ch>
2022-05-06 19:37:30 +0200

    Bigger font-size on the text only pages

--- pages/developers.vue
+++ pages/developers.vue
@@ -1,5 +1,12 @@
+<script setup>
+useHead({
+  title: "HH - Developers",
+});
+</script>
 <template>
-  <h1>API</h1>
-  Documentation on the Homebrew Hub API can be found
-  <a href="https://github.com/gbdev/homebrewhub#api-documentation">here</a>.
+  <div class="text-lg">
+    <h1>API</h1>
+    Documentation on the Homebrew Hub API can be found
+    <a href="https://github.com/gbdev/homebrewhub#api-documentation">here</a>.
+  </div>
 </template>
