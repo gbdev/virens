b0a8ea2d87c9cf285ea9cd89cf4e88a8cfa797eb
Antonio Vivace <avivace4@gmail.com>
2022-10-14 10:27:38 +0200

    footer: add digital ocean badge

--- components/footer.vue
+++ components/footer.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="layout-footer">
-    <span class="font-medium ml-2">
-      <a href="/sitemap">sitemap</a> -
-      <a href="/disclaimer">disclaimer/DMCA</a> -
-      <a href="/developers">developers</a> - <a href="/credits">credits</a> -
-      <a href="https://gbdev.io">gbdev community</a></span
-    >
+    <div>
+      <span class="font-medium ml-2">
+        <a href="/sitemap">sitemap</a> -
+        <a href="/disclaimer">disclaimer/DMCA</a> -
+        <a href="/developers">API</a> - <a href="/credits">credits</a> -
+        <a href="https://github.com/gbdev/virens">source code</a> -
+        <a href="https://gbdev.io">gbdev community</a></span
+      >
+    </div>
+    <div class="break"></div>
+    <div>
+      <a href="https://digitalocean.com" target="_blank"
+        ><img class="do-badge" src="/imgs/DO_Powered_by_Badge_white.svg"
+      /></a>
+    </div>
   </div>
 </template>
 <script>
@@ -13,3 +22,16 @@ export default {
   name: "Footer",
 };
 </script>
+<style>
+.do-badge {
+  width: 175px;
+  margin-top: 10px;
+}
+.break {
+  flex-basis: 100%;
+  height: 0;
+}
+.layout-footer {
+  flex-wrap: wrap;
+}
+</style>
