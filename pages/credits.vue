10c7f9b7ea05f7fb991ee137e955159b5c27a020
Antonio Vivace <avivace4@gmail.com>
2022-11-23 00:35:34 +0100

    Credits: add links for Binjgb and mGBA

--- pages/credits.vue
+++ pages/credits.vue
@@ -23,9 +23,12 @@ useHead({
     The <i>games archive</i> is maintained and built by the community through
     public git repositories and the datasets are released as open data. Come
     contribute!<br />
-    The GB/GBC emulation (and related code) is provided by <i>Binjgb</i>,
-    running in the browser through a webassembly build. <br /><br />Everything
-    we just listed is released as open source software. Go check out the github
+    The GB/GBC emulation is provided by <a href="https://github.com/binji/binjgb" target="_blank">Binjgb</a>,
+    running in the browser through a webassembly build. <br />
+    The GBA emulation is provided by <a href="https://mgba.io/" target="_blank">mGBA</a>,
+    running in the browser through a webassembly build. <br /><br />
+    
+    Everything just listed is released as open source software. Go check out the github
     repository, contribute and show your support to their awesome maintainers.
 
     <h3>Games</h3>
