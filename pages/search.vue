a9a287cd082372cc83c306c7b5a99ccfe7ef24cb
Antonio Vivace <avivace4@gmail.com>
2023-03-25 00:50:46 +0100

    search: prepopulate search with query params (type, platform, title)

--- pages/search.vue
+++ pages/search.vue
@@ -120,9 +120,32 @@ export default {
         });
       });
     },
+    setSearchParameters: function () {
+      // Let's check if there are some query param we should honor
+      if (this.$route.query.title) {
+        this.textQuery = this.$route.query.title
+      }
+      if (this.$route.query.platform) {
+        if (["gb", "gbc", "gba"].includes(this.$route.query.platform.toLowerCase())) {
+          this.selectedPlatform = {
+            'name': this.$route.query.platform.toUpperCase(),
+            'code': this.$route.query.platform.toUpperCase()
+          }
+        }
+      }
+      if (this.$route.query.type) {
+        if (["all", "game", "demo", "music", "tool"].includes(this.$route.query.type.toLowerCase())) {
+          this.selectedType = {
+            'name': this.$route.query.type.charAt(0).toUpperCase() + this.$route.query.type.slice(1),
+            'code': this.$route.query.type.toLowerCase()
+          }
+        }
+      }
+    },
   },
   mounted: function () {
-    console.log("mounted");
+    this.setSearchParameters()
+    this.handlesearch()
   },
   data() {
     return {
