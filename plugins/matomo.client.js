5f0098a3db916da92efcea8b8a7db8b93c45f876
Antonio Vivace <avivace4@gmail.com>
2023-12-17 23:14:48 +0100

    add matomo tracking

---
+++ plugins/matomo.client.js
@@ -0,0 +1,18 @@
+// This plugin is auto-discovered by Nuxt
+
+import { defineNuxtPlugin } from '#app'
+import VueMatomo from 'vue-matomo'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueMatomo, {
+    host: 'https://stats.gbdev.io',
+    siteId: 1, 
+    // Enables automatically registering pageviews on the router
+    router: nuxtApp.$router,
+    enableLinkTracking: true,
+    requireConsent: false,
+    trackInitialView: true,
+    disableCookies: true,
+    requireCookieConsent: false,
+  })
+})
